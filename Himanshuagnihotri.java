4ed84b008c9e41072d5633c5df430f6477884f08
Ai1996Himanshu <himanshu.ai1996@gmail.com>
2018-08-17 23:39:51 +0530

    himanshuagnihotri.java file after some changes

--- Himanshuagnihotri.java
+++ Himanshuagnihotri.java
@@ -1,27 +1,21 @@
 import java.io.*;
 import java.util.*;
-import java.util.regex.*;
-class Himanshu
-{
-  public static void main(String args[])
- {
-    Scanner sc=new Scanner(System.in);
-    int n,i;
-    ArrayList<Integer>res = new ArrayList<>();
-    String text=sc.nextLine();
-    String a[]=new  String[100];
-    n=Integer.parseInt(sc.nextLine());
-    	for(i=0;i<n;i++)
-   	 {
-      	a[i]=sc.nextLine();
-   	 }
-		for(i=0;i<n;i++)
-		{ for(int j=-1;(j=text.indexOf(a[i],j+1)) !=-1;)
-		{ res.add(j);
-		}
-		}
-	Collections.sort(res);
-	for(Integer k:res)
+class Himanshu{
+  	public static void main(String args[]){
+   	Scanner sc=new Scanner(System.in);
+    	int numofline,i;
+    	ArrayList<Integer>out_array = new ArrayList<>();
+    	String text=sc.nextLine();
+    	String tempstring[]=new  String[100];
+    	numofline=Integer.parseInt(sc.nextLine());
+    		for(i=0;i<numofline;i++){
+      		tempstring[i]=sc.nextLine();
+  		}
+	for(i=0;i<numofline;i++){
+	for(int j=-1;(j=text.indexOf(tempstring[i],j+1)) !=-1;){
+	 out_array.add(j);
+	}}
+	Collections.sort(out_array);
+	for(Integer k:out_array)
 	System.out.print(k+" ");
- }
-}
+}}
